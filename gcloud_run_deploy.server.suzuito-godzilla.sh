9f0e5cae502efd8a59594a372ee46c4fab2ea8a9
鈴木泰斗 <suzukitaito@ip-192-168-11-3.ap-northeast-1.compute.internal>
2021-10-29 21:53:00 +0900

    save

---
+++ gcloud_run_deploy.server.suzuito-godzilla.sh
@@ -0,0 +1,12 @@
+gcloud builds submit --config=./cloudbuild.server.suzuito-godzilla.yaml
+gcloud run deploy blog1-server-go \
+--image=gcr.io/suzuito-godzilla/blog1-server-go:latest \
+--platform=managed \
+--region=asia-northeast1 \
+--allow-unauthenticated \
+--memory=512Mi \
+--cpu=1000m \
+--max-instances=1 \
+--set-env-vars="ENV=godzilla" \
+--set-env-vars="PRERENDER_URL=https://prerendering-iflkmd4llq-an.a.run.app" \
+--set-env-vars="FRONT_URL=https://suzuito-godzilla-blog1-server.storage.googleapis.com"
--- gcloud_run_deploy.server.suzuito-minilla.sh
+++ gcloud_run_deploy.server.suzuito-minilla.sh
@@ -7,6 +7,6 @@ gcloud run deploy blog1-server-go \
 --memory=512Mi \
 --cpu=1000m \
 --max-instances=1 \
---set-env-vars="ENV=dev" \
+--set-env-vars="ENV=minilla" \
 --set-env-vars="PRERENDER_URL=https://prerendering-fgfw3lzk2a-an.a.run.app" \
 --set-env-vars="FRONT_URL=https://suzuito-minilla-blog1-server.storage.googleapis.com"
