9b9c7892bfa8ec25aea395117f7a1055aa661984
鈴木泰斗 <suzukitaito@ip-192-168-11-3.ap-northeast-1.compute.internal>
2021-10-29 21:45:16 +0900

    save

---
+++ gcloud_run_deploy.server.suzuito-minilla.sh
@@ -0,0 +1,12 @@
+
+gcloud run deploy blog1-server-go \
+--image=gcr.io/suzuito-minilla/blog1-server-go:latest \
+--platform=managed \
+--region=asia-northeast1 \
+--allow-unauthenticated \
+--memory=512Mi \
+--cpu=1000m \
+--max-instances=1 \
+--set-env-vars="ENV=dev" \
+--set-env-vars="PRERENDER_URL=https://prerendering-fgfw3lzk2a-an.a.run.app" \
+--set-env-vars="FRONT_URL=https://suzuito-minilla-blog1-server.storage.googleapis.com"
