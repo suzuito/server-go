aca037354014ee464d076023e5aabf42a36fc944
suzukitaito <suzukitaito@suzukitaitonoMacBook-Pro.local>
2022-02-14 08:34:09 +0900

    Add sitema

--- internal/server/handler_test.go
+++ internal/server/handler_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
+	"github.com/suzuito/server-go/internal/inject"
 	"github.com/suzuito/server-go/internal/setting"
 	"github.com/suzuito/server-go/internal/usecase"
 )
@@ -73,6 +73,15 @@ func TestHandlerBlog(t *testing.T) {
 					ServeHTTP(gomock.Any(), gomock.Any())
 			},
 		},
+		{
+			desc:            `成功 /sitemap.xmlはサイトマップへ`,
+			inputHTTPMethod: http.MethodGet,
+			inputURL:        "http://foo.co.jp/sitemap.xml",
+			setUp: func(dep *usecase.DependsMock) {
+				dep.ReverseProxySitemap.EXPECT().
+					ServeHTTP(gomock.Any(), gomock.Any())
+			},
+		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
@@ -82,136 +91,15 @@ func TestHandlerBlog(t *testing.T) {
 			env := setting.Environment{
 				Env: "dev",
 			}
-			recorder := httptest.NewRecorder()
-			h := Handler(&env, usecase.NewProxyBlog(
-				dep.HealthCheckBotMatcher,
-				dep.ExternalAppBotMatcher,
-				dep.ReverseProxyFront,
-				dep.ReverseProxyPrerendering,
-				env.Env,
-			))
-			req, err := http.NewRequest(tC.inputHTTPMethod, tC.inputURL, nil)
-			if err != nil {
-				t.Error(err)
-				return
-			}
-			h(recorder, req)
-		})
-	}
-}
-
-func newRoutes(t *testing.T, c int) ([]*usecase.MockReverseProxyRoute, *usecase.MockReverseProxyRoute, func()) {
-	ctrls := []*gomock.Controller{}
-	routes := []*usecase.MockReverseProxyRoute{}
-	for i := 0; i < c; i++ {
-		ctrl := gomock.NewController(t)
-		ctrls = append(ctrls, ctrl)
-		route := usecase.NewMockReverseProxyRoute(ctrl)
-		routes = append(routes, route)
-	}
-	ctrlDefault := gomock.NewController(t)
-	routeDefault := usecase.NewMockReverseProxyRoute(ctrlDefault)
-	return routes, routeDefault, func() {
-		for _, ctrl := range ctrls {
-			ctrl.Finish()
-		}
-		ctrlDefault.Finish()
-	}
-}
-
-func TestNoRoutesHandler(t *testing.T) {
-	pxy := usecase.ReverseProxyRoutes{
-		Routes:       []usecase.ReverseProxyRoute{},
-		DefaultRoute: nil,
-	}
-	req, err := http.NewRequest(http.MethodGet, "http://localhost:8080", nil)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	recorder := httptest.NewRecorder()
-	Handler(&setting.Environment{Env: "dev"}, &pxy)(recorder, req)
-	assert.Equal(t, http.StatusNotFound, recorder.Result().StatusCode)
-}
-
-func TestHandlerV2(t *testing.T) {
-	testCases := []struct {
-		desc            string
-		inputHTTPMethod string
-		inputURL        string
-		inputC          int
-		setUp           func(routes []*usecase.MockReverseProxyRoute, routeDefault *usecase.MockReverseProxyRoute)
-	}{
-		{
-			desc:            `Routes:0, DefaultRoute:not nil`,
-			inputHTTPMethod: http.MethodGet,
-			inputURL:        "http://localhost:8080",
-			inputC:          0,
-			setUp: func(routes []*usecase.MockReverseProxyRoute, routeDefault *usecase.MockReverseProxyRoute) {
-				routeDefault.EXPECT().ServeHTTP(gomock.Any(), gomock.Any())
-			},
-		},
-		{
-			desc:            `Routes:true,false, DefaultRoute:not nil`,
-			inputHTTPMethod: http.MethodGet,
-			inputURL:        "http://localhost:8080",
-			inputC:          2,
-			setUp: func(routes []*usecase.MockReverseProxyRoute, routeDefault *usecase.MockReverseProxyRoute) {
-				routes[0].EXPECT().
-					Check(gomock.Any()).Return(true)
-				routes[0].EXPECT().
-					ServeHTTP(gomock.Any(), gomock.Any())
-			},
-		},
-		{
-			desc:            `Routes:false,true, DefaultRoute:not nil`,
-			inputHTTPMethod: http.MethodGet,
-			inputURL:        "http://localhost:8080",
-			inputC:          2,
-			setUp: func(routes []*usecase.MockReverseProxyRoute, routeDefault *usecase.MockReverseProxyRoute) {
-				routes[0].EXPECT().
-					Check(gomock.Any()).Return(false)
-				routes[1].EXPECT().
-					Check(gomock.Any()).Return(true)
-				routes[1].EXPECT().
-					ServeHTTP(gomock.Any(), gomock.Any())
-			},
-		},
-		{
-			desc:            `Routes:false,false, DefaultRoute:not nil`,
-			inputHTTPMethod: http.MethodGet,
-			inputURL:        "http://localhost:8080",
-			inputC:          2,
-			setUp: func(routes []*usecase.MockReverseProxyRoute, routeDefault *usecase.MockReverseProxyRoute) {
-				routes[0].EXPECT().
-					Check(gomock.Any()).Return(false)
-				routes[1].EXPECT().
-					Check(gomock.Any()).Return(false)
-				routeDefault.EXPECT().
-					ServeHTTP(gomock.Any(), gomock.Any())
-			},
-		},
-	}
-	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
-			routes, routeDefault, closeFunc := newRoutes(t, tC.inputC)
-			defer closeFunc()
-			tC.setUp(routes, routeDefault)
-			env := setting.Environment{
-				Env: "dev",
-			}
-			tempRoutes := []usecase.ReverseProxyRoute{}
-			for _, r := range routes {
-				tempRoutes = append(tempRoutes, r)
+			gdep := inject.GlobalDepends{
+				ExternalAppBotMatcher: dep.ExternalAppBotMatcher,
+				HealthCheckBotMatcher: dep.HealthCheckBotMatcher,
+				ReverseProxyPrerender: dep.ReverseProxyPrerendering,
+				ReverseProxyFront:     dep.ReverseProxyFront,
+				ReverseProxySitemap:   dep.ReverseProxySitemap,
 			}
 			recorder := httptest.NewRecorder()
-			h := Handler(
-				&env,
-				&usecase.ReverseProxyRoutes{
-					Routes:       tempRoutes,
-					DefaultRoute: routeDefault,
-				},
-			)
+			h := HandlerBlog(&env, &gdep)
 			req, err := http.NewRequest(tC.inputHTTPMethod, tC.inputURL, nil)
 			if err != nil {
 				t.Error(err)
--- internal/usecase/depends_mock.go
+++ internal/usecase/depends_mock.go
@@ -9,6 +9,7 @@ import (
 type DependsMock struct {
 	ExternalAppBotMatcher    *MockUserAgentMatcher
 	HealthCheckBotMatcher    *MockUserAgentMatcher
+	ReverseProxySitemap      *MockReverseProxy
 	ReverseProxyFront        *MockReverseProxy
 	ReverseProxyPrerendering *MockReverseProxy
 }
@@ -19,14 +20,17 @@ func NewDependsMock(t *testing.T) (*DependsMock, func()) {
 	ctrlHealthCheckBotMatcher := gomock.NewController(t)
 	ctrlReverseProxyFront := gomock.NewController(t)
 	ctrlReverseProxyPrerendering := gomock.NewController(t)
+	ctrlReverseProxySitemap := gomock.NewController(t)
 	r.ExternalAppBotMatcher = NewMockUserAgentMatcher(ctrlExternalAppBotMatcher)
 	r.HealthCheckBotMatcher = NewMockUserAgentMatcher(ctrlHealthCheckBotMatcher)
 	r.ReverseProxyFront = NewMockReverseProxy(ctrlReverseProxyFront)
 	r.ReverseProxyPrerendering = NewMockReverseProxy(ctrlReverseProxyPrerendering)
+	r.ReverseProxySitemap = NewMockReverseProxy(ctrlReverseProxySitemap)
 	return &r, func() {
 		ctrlExternalAppBotMatcher.Finish()
 		ctrlHealthCheckBotMatcher.Finish()
 		ctrlReverseProxyFront.Finish()
 		ctrlReverseProxyPrerendering.Finish()
+		ctrlReverseProxySitemap.Finish()
 	}
 }
